1a695b98821cff757c476476461f059ff90c3f10
ikari <ikari1607@gmail.com>
2018-08-28 20:49:35 -0300

    parte2ExamenSantiago

--- src/main/java/ta_te_ti/Tateti.java
+++ src/main/java/ta_te_ti/Tateti.java
@@ -6,11 +6,20 @@ public class Tateti {
         Jugada jugadas = new Jugada();
         Tablero miTablero = new Tablero();
         int contador = 0;
+        boolean bandera = false;
+        int campodejuego[] = miTablero.retornoArray();
         while (contador == 9) {
             int valor = jugadas.pedirNumero();
-            miTablero.elTablero(valor);
+            if(campodejuego[valor] != 0){
+                bandera = true;
+            }
+            if(bandera==false){
+               miTablero.elTablero(valor); 
+            } else {
+                System.out.println("la posicion esta ocupada");
+            }
             contador++;
         }
-
+        
     }
 }

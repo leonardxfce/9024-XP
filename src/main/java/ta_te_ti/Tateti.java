ac3989e6be75fffdbbafb459012ea1693b8a9c35
ikari <ikari1607@gmail.com>
2018-08-28 18:28:17 -0300

    no anda pero es un tateti

--- src/main/java/ta_te_ti/Tateti.java
+++ src/main/java/ta_te_ti/Tateti.java
@@ -1,5 +1,16 @@
 package ta_te_ti;
 
 public class Tateti {
-    
+
+    public static void main(String[] args) {
+        Jugada jugadas = new Jugada();
+        Tablero miTablero = new Tablero();
+        int contador = 0;
+        while (contador == 9) {
+            int valor = jugadas.pedirNumero();
+            miTablero.elTablero(valor);
+            contador++;
+        }
+
+    }
 }
---
+++ src/test/java/ta_te_ti/testTablero.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ta_te_ti;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author SantiagoGuirado
+ */
+public class testTablero extends TestCase {
+    
+    public testTablero(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    public void testTablero() {
+        System.out.println("test de tablero:");
+        Tablero instance = new Tablero();
+        int valor = 1;
+        boolean expectativa = true;
+        boolean respuesta = instance.elTablero(valor);
+        assertEquals(expectativa, respuesta);
+    }
+}

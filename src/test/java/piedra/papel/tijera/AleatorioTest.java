50b499bfc503f4f20a0952c34c5c0bdbb3d594e5
andreahoran <caro_barroso1998@hotmail.com>
2018-08-28 19:50:23 -0300

    primer commit

---
+++ src/test/java/piedra/papel/tijera/AleatorioTest.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piedra.papel.tijera;
+
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author caro_
+ */
+public class AleatorioTest extends TestCase{
+    
+    public AleatorioTest() {
+    }
+    
+    
+
+    /**
+     * Test of aleatorio method, of class Aleatorio.
+     */
+    /*@Test
+    public void testAleatorio() {
+        System.out.println("aleatorio");
+        String dato = "papel";
+        Aleatorio instance = new Aleatorio();
+        String expResult = "gano";
+        String result = instance.aleatorio(dato);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+     @Test
+    public void testAleatorio01() {
+        System.out.println("aleatorio");
+        String dato = "piedra";
+        Aleatorio instance = new Aleatorio();
+        String expResult = "perdió";
+        String result = instance.aleatorio(dato);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    @Test
+    public void testAleatorio02() {
+        System.out.println("aleatorio");
+        String dato = "tijera";
+        Aleatorio instance = new Aleatorio();
+        String expResult = "gano";
+        String result = instance.aleatorio(dato);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }*/
+    
+    public void testAleatorio03() {
+        System.out.println("aleatorio");
+        String dato = "tijera";
+        Aleatorio_Tijera instance = new Aleatorio_Tijera();
+        String expResult = "tijera";
+        String result = instance.tijeraEj(dato);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+ 
+    public void testAleatorio04() {
+        System.out.println("aleatorio");
+        String dato = "piedra";
+        Aleatorio_Piedra instance = new Aleatorio_Piedra();
+        String expResult = "piedra";
+        String result = instance.piedraEj(dato);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+  
+    public void testAleatorio05() {
+        System.out.println("aleatorio");
+        String dato = "papel";
+        Aleatorio_Papel instance = new Aleatorio_Papel();
+        String expResult = "papel";
+        String result = instance.papelEj(dato);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}

8366a8681132920c2432c66ffbadd06a77908f84
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-08-28 22:36:02 -0300

    intentanto pegar coverrarls

--- src/test/java/com/ies/app/AppTest.java
+++ src/test/java/com/ies/app/AppTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 public class AppTest extends TestCase {
 
     public void test_de_ejemplo() {
+        System.out.println("Algo");
         assertTrue(true);
     }
 }

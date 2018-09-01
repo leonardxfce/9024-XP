b24f4b767b10ea7d9f374f29b40e3cfc27d31e3c
andreahoran <caro_barroso1998@hotmail.com>
2018-08-31 21:03:07 -0300

    funcionando

--- src/main/java/piedra_papel_tijera/PiedraPapelTijera.java
+++ src/main/java/piedra_papel_tijera/PiedraPapelTijera.java
@@ -5,6 +5,8 @@
  */
 package piedra_papel_tijera;
 
+import java.util.Scanner;
+
 /**
  *
  * @author caro_
@@ -15,7 +17,40 @@ public class PiedraPapelTijera {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        String dato;
+        int numA;
+        System.out.println("******** PIEDRA, PAPEL O TIJERA ********");
+        System.out.println("Ingrese su opción: ");
+        dato = leer.nextLine();
+        JugadaComputadora nuevaJugada = new JugadaComputadora();
+        numA = nuevaJugada.Aleatorio();
+        ComprobarJugada comp = new ComprobarJugada();
+        //String resultado []= new String [3];
+        String resultado = comp.comprobar(numA, dato);
+        String resultado1 = comp.computadoraResult(numA);
+        System.out.println(resultado1);
+        System.out.println("Usted "+ resultado);
+        /*for(int i = 0; i < 3; i++){
+            System.out.println("Ingrese su opción: ");
+            dato = leer.nextLine();
+            numA = nuevaJugada.Aleatorio();
+            System.out.println("jugada de la computadora: "+numA);
+            resultado[i]= comp.comprobar(numA, dato);
+        }
+        int resultDefinitivo = 0;
+        for(int j = 0; j<3;j++){
+            if(resultado[0]==resultado[1]){
+                resultDefinitivo =resultDefinitivo + 0;
+            }else{
+                resultDefinitivo = resultDefinitivo + 1;
+            }
+        }
+        if(resultDefinitivo>=1){
+            System.out.println("Usted Ganó");
+        }else{
+            System.out.println("Usted Perdió");
+        }*/
     }
     
 }

d3c28636833d9c6008477594129c159f503803ad
lauta <lauta555@hotmail.com>
2018-09-04 20:47:25 -0300

    LautaroyGregoris

---
+++ src/test/java/numero_magico/Jugador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numero_magico;
+
+
+class Jugador{
+ int vidas;
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
+
+    void quitarVida() {
+        this.vidas--;
+    }
+
+    public Jugador() {
+        this.vidas=10;
+    }
+    
+  
+}

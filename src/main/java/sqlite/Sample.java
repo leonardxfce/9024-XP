744a257ecc9d7262d531af4a1be9aee99bc4c0a4
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-09-07 19:21:05 -0300

    sumo librerias de bases de datos

--- src/main/java/sqlite/Sample.java
+++ src/main/java/sqlite/Sample.java
@@ -1,4 +1,5 @@
 package sqlite;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -6,33 +7,22 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Sample {
-  public String conexionEjemplo() {
-    Connection connection = null;
-    try {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30); // set timeout to 30 sec.
-      statement.executeUpdate("drop table if exists person");
-      statement.executeUpdate("create table person (id integer, name string)");
-      statement.executeUpdate("insert into person values(1, 'leo')");
-      statement.executeUpdate("insert into person values(2, 'yui')");
-      ResultSet rs = statement.executeQuery("select * from person");
-      rs.next();
-      return rs.getString("name");
-    } catch (SQLException e) {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
-    } finally {
-      try {
-        if (connection != null)
-          connection.close();
-      } catch (SQLException e) {
-        // connection close failed.
-        System.err.println(e);
-      }
+
+    public String conexionEjemplo() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            rs.next();
+            return rs.getString("name");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return "fail";
     }
-    return "fail";
-  }
 }

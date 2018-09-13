d5c53fbedd388cfcbcc6f22f32e3083d50e1c6d9
TheBoudica <theboudica98@gmail.com>
2018-09-12 21:34:58 -0300

    para jugar contra la IA

---
+++ src/main/java/ta_te_ti/IA.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ta_te_ti;
+
+/**
+ *
+ * @author TheBoudica
+ */
+public class IA {
+    
+    public int alatoriedad(){
+        int intermedio= (9-1)+1;
+        int jugadaDeLaPC= (int)(Math.random()*intermedio)+1;
+        return jugadaDeLaPC;
+    }
+    
+}
---
+++ src/test/java/ta_te_ti/IATest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ta_te_ti;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author thebo
+ */
+public class IATest {
+    
+    public IATest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of alatoriedad method, of class IA.
+     */
+    @Test
+    public void testAlatoriedad() {
+        System.out.println("alatoriedad");
+        IA instance = new IA();
+        int result = instance.alatoriedad();
+        System.out.println(result);
+        assertTrue(0<result  &&  result<10);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}

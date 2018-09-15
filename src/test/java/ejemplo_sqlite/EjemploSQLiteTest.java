558d469d67861431e99e94ca39670333ba1efd2d
leonardxfce <leonardo.araoz.dev@gmail.com>
2018-09-15 10:57:02 -0300

    remove one folder and improve coverage

---
+++ src/test/java/ejemplo_sqlite/EjemploSQLiteTest.java
@@ -0,0 +1,21 @@
+package ejemplo_sqlite;
+
+import ejemplo_sqllite.EjemploSQLite;
+import junit.framework.TestCase;
+
+public class EjemploSQLiteTest extends TestCase {
+
+    public void test_conecta_a_bd() {
+        EjemploSQLite instance = new EjemploSQLite();
+        String result = instance.conexionEjemplo("jdbc:sqlite:sample.db");
+        String expected = "leo";
+        assertEquals(result,expected);
+    }
+    public void test_falla_al_conectar() {
+        EjemploSQLite instance = new EjemploSQLite();
+        String result = instance.conexionEjemplo("pepito");
+        String expected = "fail";
+        assertEquals(result,expected);
+    }
+
+}

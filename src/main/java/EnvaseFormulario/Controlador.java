0e337cd24798438711787c4a2df3680469d7e419
Lisa <lisaevemarin17@gmail.com>
2018-09-14 22:23:13 -0300

    java fx primer envase

---
+++ src/main/java/EnvaseFormulario/Controlador.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EnvaseFormulario;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lisa
+ */
+public class Controlador implements EventHandler<ActionEvent> {
+
+    ForEnvase formulario;
+
+    public Controlador(Stage primaryStage) {
+        formulario = new ForEnvase();
+        formulario.start(primaryStage);
+        formulario.aceptar.setOnAction(this);
+    }
+
+    @Override
+    public void handle(ActionEvent pepito) {
+        System.out.println("ME HICIERON CLICK");
+    }
+
+}

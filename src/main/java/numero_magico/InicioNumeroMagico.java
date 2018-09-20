d48749edcc66590bd0c683cc5be83a60f4d00895
OlmosLautaroGaston <lautaolmoss@gmail.com>
2018-09-19 21:36:19 -0300

    finishgame

--- src/main/java/numero_magico/InicioNumeroMagico.java
+++ src/main/java/numero_magico/InicioNumeroMagico.java
@@ -29,16 +29,19 @@ public class InicioNumeroMagico {
             int conter = 1;
             String resultado;
             System.out.println("Bienvenidos a Numeros Magicos");
+            System.out.println("OBJETIVOS DEL JUEGO");
+            System.out.println("*Usted debe adivinar el número magico");
+            System.out.println("*El número está entre 1 y 100");
             do {
                 vidas = jugV.getVidas();
                 jugV.quitarVida();
-                System.out.println("Usted tiene un total de " + vidas + " vidas");
+                System.out.println("*Usted tiene un total de " + vidas + " vidas");
                 Scanner numUsuario = new Scanner(System.in);    //funcion q crea el scanner del NumUser
                 String userN = numUsuario.nextLine();   //leer Numero de Usuario = userN
                 int userInt = Integer.parseInt(userN);  //Transforma el dato StringUser a Integer
                 resultado = numM.compararNumeros(userInt);   //llama al metodo que Compara numeroUser con NumeroMagicoAleatorio
                 System.out.println("el numero mágico es " + resultado);
-            } while (resultado != "Igual" && vidas != 0);
+            } while (resultado != "Igual" && vidas != 1);
             System.out.println("El juego a terminado :D");
             baseDeDatos obj = new baseDeDatos();
             obj.conexionEjemplo(vidas);
@@ -47,22 +50,47 @@ public class InicioNumeroMagico {
             // la pc dice un numero
             // bucle comprobar que la pc adivina le numero
             JugadorPC pc = new JugadorPC();
+            Jugador jugV = new Jugador(); //llamamos la clase jugador
+            int vidas;
+            int conter = 1;
+            
+            System.out.println("Bienvenido a Numero Mágico Inverso");
+            System.out.println("OBJETIVOS DEL JUEGO:");
+            System.out.println("*Piense en un número del 1 al 100 y la máquina intentará adivinarlo");
+            System.out.println("*Ingrese menor '<' mayor '>' o igual '=' segun corresponda");
+            
             int min = 0;
             int max = 100;
-            int numeroPC = pc.darNumero(min, max);
-            Scanner nomb = new Scanner(System.in);
-            String otroNombre = nomb.nextLine();
-            if (otroNombre == "<") {
-                max = numeroPC;
-            }
-            if (otroNombre == ">") {
-                min = numeroPC;
-            }
-            if (otroNombre == "=") {
-                System.out.println("Fin del juego, Tu numero es" + numeroPC);
-            }
+            int numeroPC;
+            String otroNombre;
+            do {
+                vidas = jugV.getVidas();
+                jugV.quitarVida();
+                System.out.println("*La Maquina tiene un total de " + vidas + " vidas");
+                numeroPC = pc.darNumero(min, max);
+                System.out.println("Tu número es "+numeroPC+"?");
+                Scanner nomb = new Scanner(System.in);
+                 otroNombre = nomb.nextLine();
 
-        }
+                if (otroNombre.equals("<")) {
+                    max = numeroPC;
+                }
+                if (otroNombre.equals(">")) {
+                    min = numeroPC;
+                }
+                if (otroNombre.equals("=")) {
+                    
+                    System.out.println("Fin del juego, Tu numero es " + numeroPC);
+                }
+                
+            } while (!otroNombre.equals("=") && vidas != 1);
+            if (vidas==1) {
+                System.out.println("Fin del juego, La computadora se quedo sin vida");
+                }
+            }
+            
+
+  
     }
 
     public int testeo(int option) {

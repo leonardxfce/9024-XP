4508e0ff7dc992efd85314e207908de9a811a875
OlmosLautaroGaston <lautaolmoss@gmail.com>
2018-09-26 21:43:53 -0300

    selectfinish:

--- src/main/java/login/ModeloLogin.java
+++ src/main/java/login/ModeloLogin.java
@@ -7,6 +7,7 @@ package login;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -17,35 +18,51 @@ import java.util.logging.Logger;
  * @author lauta
  */
 public class ModeloLogin {
+
     int id;
     String usuario;
     String password;
     String url;
     Connection connection;
     Statement statement;
-    public ModeloLogin(String usuario,String password){
-    this.usuario = usuario;
-    this.password = password;
-    this.url = "jdbc:sqlite:sample.db";
-    
-    
+    ResultSet rs;
+
+    public ModeloLogin(String usuario, String password) {
+        this.usuario = usuario;
+        this.password = password;
+        this.url = "jdbc:postgresql://pellefant.db.elephantsql.com:5432/prafqulb";
+
     }
-    public void seleccionar(){
-        
-    }
-    public void insertar(){
+
+    public String seleccionar() {
         try {
-            connection = DriverManager.getConnection(url);
+            connection = DriverManager.getConnection(url, "prafqulb", "M-dsT1RJ6AM7h17OM2PppJ2-Z5TPIQTc");
             statement = connection.createStatement();
-            statement.executeUpdate("insert into USUARIOS values(1, '"+usuario+"','"+password+"')");
+            rs = statement.executeQuery("SELECT * FROM USUARIOS where USUARIO = '" + usuario + "' AND CONTRASENA = '" + password + "';");
+            rs.next();
+            return rs.getString("USUARIO");
+        } catch (SQLException ex) {
+            Logger.getLogger(ModeloLogin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public void insertar() {
+        try {
+
+            connection = DriverManager.getConnection(url, "prafqulb", "M-dsT1RJ6AM7h17OM2PppJ2-Z5TPIQTc");
+            statement = connection.createStatement();
+            statement.executeUpdate("insert into USUARIOS values( 2,'" + usuario + "'," + password + ");");
         } catch (SQLException ex) {
             Logger.getLogger(ModeloLogin.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     public void eliminar() {
-        
+
     }
+
     public void modificar() {
-        
+
     }
 }
